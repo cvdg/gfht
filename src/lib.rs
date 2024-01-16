a9ec3f90af5ff9507bea49d81d101eb4bbbd830f
Cees van de Griend <cees.van.de.griend@protonmail.com>
2024-01-16 11:42:43 +0100

    14. Longest Common Prefix: start

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod s0001_two_sum;
 mod s0009_is_palindrome;
 mod s0013_roman_to_int;
+mod s0014_longest_common_prefix;
---
+++ src/s0014_longest_common_prefix.rs
@@ -0,0 +1,67 @@
+/// https://leetcode.com/problems/longest-common-prefix/
+//
+// Write a function to find the longest common prefix string amongst an array of strings.
+//
+// If there is no common prefix, return an empty string "".
+//
+//
+// Example 1:
+//
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+//
+//
+// Example 2:
+//
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+//
+//
+
+struct Solution;
+
+// === submission begin ========================================
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        "".to_string()
+    }
+}
+
+// === submission end ==========================================
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sanity() {
+        assert_eq!(2, 1 + 1);
+    }
+    
+    #[test]
+    fn example1() {
+        assert_eq!(
+            "fl",
+            Solution::longest_common_prefix(vec![
+                "flower".to_string(),
+                "flow".to_string(),
+                "flight".to_string()
+            ])
+        );
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(
+            "",
+            Solution::longest_common_prefix(vec![
+                "".to_string(),
+                "racecar".to_string(),
+                "car".to_string()
+            ])
+        );
+    }
+}
