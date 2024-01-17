e1eab1cee957e0b2ac77ba6b32b22aab45b8c0a9
Cees van de Griend <cees.van.de.griend@protonmail.com>
2024-01-17 14:37:11 +0100

    s0014: refactored

--- src/s0014_longest_common_prefix.rs
+++ src/s0014_longest_common_prefix.rs
@@ -1,4 +1,6 @@
-/// https://leetcode.com/problems/longest-common-prefix/
+#[allow(dead_code)]
+
+// https://leetcode.com/problems/longest-common-prefix/
 //
 // Write a function to find the longest common prefix string amongst an array of strings.
 //
@@ -24,32 +26,26 @@ pub struct Solution;
 // === submission begin ========================================
 
 impl Solution {
-    #[allow(dead_code)]
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
         let mut prefix = String::new();
-        let mut same = true;
 
         // loop over all charactes in the first string
         for (i1, c1) in strs[0].char_indices() {
             // loop over strings from the second
             for s in &strs[1..] {
+                // string shorter
                 if s.len() <= i1 {
-                    same = false;
-                    break;
+                    return prefix;
                 } else {
                     let c2 = s.chars().nth(i1).unwrap();
+                    // different character
                     if c1 != c2 {
-                        same = false;
-                        break;
+                        return prefix;
                     }
                 }
             }
 
-            if same {
-                prefix.push(c1);
-            } else {
-                break;
-            }
+            prefix.push(c1);
         }
         prefix
     }
